8b7063c451aacefd722596fdc8c737d0bb1b605a
talitapavlova <talitapavlova@gmail.com>
2018-03-16 13:11:27 +0100

    loop for storing sequence, needs debuging!

--- AsmMemoGame/AsmMemoGame/main.asm
+++ AsmMemoGame/AsmMemoGame/main.asm
@@ -18,22 +18,56 @@
 	out spl, r17
 
 		; configuration of variables
-.equ loop_delay_counter1 = 25
+.equ loop_delay_counter1 = 25				; creating variable to hold the counter for the most inner loop in the delay method
+
+start:										; game begins
+											; initial game setup
+.equ seq_counter = 3						; variable to count the number of steps in the sequence, initially set to 3
+.equ sequence = 0x03						; giving an adress for the sequence in the ram (0x200)
+.equ seq_value = 1							; setting the value that will go into the sequence (this should be random later on)
+
+	ldi r18, seq_counter					; the sequence counter loaded into r18
+		
+
+		; loop to load the initial 3 values in sequence
+		; for (int i = 0; i < seqCounter; i++) {
+		;	sequence[i] = ranGen.nextInt(9);
+		;}
+
+		; problem with the loop, it always stores the value in the same address: how to increment the adress? 
+		; when we do we have to transfer the sequence in ram
+
+	ldi r17, 0								; load the counter for the loop into r17								
+	ldi r16, seq_value						; load the sequence value into r16
+	ldi r20, sequence						; load the initial adress from where the sequence starts into r20
+seq_loading:
+	sts sequence, r16						; store one sequence value 
+	inc r20									; increment the address to store the next sequence value (does not work!!!!!!!)
+	inc r16									; give new value to the variable value (this should be random later on)
+	inc r17									; increment loop counter
+	cp r17, r18								; compare loop counter with seq counter
+	brlo seq_loading						; jump to seq_loading if loop counter < seq counter
+
+
+
+
+
+	jmp start								; game is restarted
 
 
 delay:										; creating a method delay to be called when needed
-	ldi r18, 255							; counter for loop1
+	ldi r29, 255							; load counter for loop1 into r29
 loop1:
-	ldi r19, 255							; counter for loop2
+	ldi r30, 255							; load counter for loop2 into r30
 loop2:
-	ldi r20, loop_delay_counter1			; counter for loop3
+	ldi r31, loop_delay_counter1			; load counter for loop3 into r31
 loop3:
-	dec r20									; decrement counter for loop3
-	brne loop3								; jump to loop3 label if r20 != 0
-	dec r19									; decrement counter for loop2
-	brne loop2								; jump to loop2 label if r19 != 0
-	dec r18									; decrement counter for loop1
-	brne loop1								; jump to loop1 label if r18 != 0
+	dec r31									; decrement counter for loop3
+	brne loop3								; jump to loop3 label if r31 != 0
+	dec r30									; decrement counter for loop2
+	brne loop2								; jump to loop2 label if r30 != 0
+	dec r29									; decrement counter for loop1
+	brne loop1								; jump to loop1 label if r29 != 0
 	ret
 
 
